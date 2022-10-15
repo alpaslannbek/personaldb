1662d6d26f50049c321dd73c85374c25da2010bb
alpaslannbek <alpaslannbek@gmail.com>
2022-10-15 22:41:07 +0200

    Item Endpoints initial version

--- src/main/java/com/personaldb/PersonalDBApplication.java
+++ src/main/java/com/personaldb/PersonalDBApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PersonaldbApplication {
+public class PersonalDBApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PersonaldbApplication.class, args);
+		SpringApplication.run(PersonalDBApplication.class, args);
 	}
 
 }
