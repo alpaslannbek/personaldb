c25ed736603c5503c137f5d8e9244f6932475fb2
alpaslannbek <alpaslannbek@gmail.com>
2022-10-15 22:11:01 +0200

    Inital Commit

---
+++ src/main/java/com/personaldb/PersonaldbApplication.java
@@ -0,0 +1,13 @@
+package com.personaldb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaldbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaldbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personaldb/PersonaldbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personaldb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaldbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
